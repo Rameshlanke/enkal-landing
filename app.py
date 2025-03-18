530b253548658cdecc93e89b6870894b06d571dd
rameshlanke <66162970+Rameshlanke@users.noreply.github.com>
2025-03-18 11:49:02 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+    
+@app.route('/terms')
+def terms():
+    return render_template('t&c.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
